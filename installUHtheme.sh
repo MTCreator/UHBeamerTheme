4acb9a37dec73ec9b400cac501d768bea6b858d9
Mike Vandersanden <mike1999v@gmail.com>
2018-12-13 11:26:29 +0100

    light theme

--- installUHtheme.sh
+++ installUHtheme.sh
@@ -4,4 +4,5 @@
 mkdir -p ~/texmf/tex/latex/beamer/themes
 
 cp theme/beamerthemeUHBeamer.sty ~/texmf/tex/latex/beamer/themes/beamerthemeUHBeamer.sty
+cp theme/beamerthemeUHBeamerLight.sty ~/texmf/tex/latex/beamer/themes/beamerthemeUHBeamerLight.sty
 
