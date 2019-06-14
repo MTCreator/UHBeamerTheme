13b94963640ecec7528226e491dbb2d71756301b
Mike Vandersanden <mike1999v@gmail.com>
2019-06-14 14:07:39 +0200

    added poster theme

--- installUHtheme.sh
+++ installUHtheme.sh
@@ -6,4 +6,4 @@ mkdir -p ~/texmf/tex/latex/beamer/themes
 
 cp theme/beamerthemeUHBeamer.sty ~/texmf/tex/latex/beamer/themes/beamerthemeUHBeamer.sty
 cp theme/beamerthemeUHBeamerLight.sty ~/texmf/tex/latex/beamer/themes/beamerthemeUHBeamerLight.sty
-
+cp theme/beamerthemeUHBeamerPoster.sty ~/texmf/tex/latex/beamer/themes/beamerthemeUHBeamerPoster.sty
